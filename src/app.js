3f68ada29cccc52ae27543ee4d505b4f40e7026b
Rat <rat4.email@gmail.com>
2021-10-04 11:58:48 -0500

    add spanish location to date picker

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import React from 'react'
 import DateAdapter from '@mui/lab/AdapterMoment'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
 import { ThemeProvider } from '@mui/material/styles'
-
+import 'moment/locale/es'
 import theme from './theme'
 import Form from './components/form'
 
 const App = () => {
   return <ThemeProvider theme={theme}>
-    <LocalizationProvider dateAdapter={DateAdapter}>
+    <LocalizationProvider dateAdapter={DateAdapter} locale="es">
         <Form/>
     </LocalizationProvider>
   </ThemeProvider>
